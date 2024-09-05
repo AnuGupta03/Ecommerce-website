8d18a1c862b5909a362fcc6375afe4ea3707edeb
Anuradha Gupta <anuradhagwl98@gmail.com>
2024-09-05 09:52:55 +0530

    New Code

---
+++ admin/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Admin/>
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ListProduct.css'
+const ListProduct = () => {
+  return (
+    <div className='list-product'>
+    </div>
+  )
+}
+
+export default ListProduct
